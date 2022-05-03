560a1b92a0a42060264f805d1a87f5445a3493f6
yjcho <jinjanic91@gmail.com>
2022-05-03 22:37:46 +0900

    feat. validating create user  done

--- src/auth/dto/authCredential.dto.ts
+++ src/auth/dto/authCredential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @MaxLength(20)
+  @MinLength(4)
+  @IsString()
   username: string;
+
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only accepts english and number',
+  })
+  @MaxLength(16)
+  @MinLength(4)
+  @IsString()
   password: string;
 }

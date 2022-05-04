beef93134cd0d5af127e99fa969eca73dc1e6486
yjcho <jinjanic91@gmail.com>
2022-05-04 23:33:44 +0900

    feat. unique column setting done

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/authCredential.dto';
 import { User } from './user.entity';
@@ -7,6 +11,14 @@ export class AuthRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Already Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }

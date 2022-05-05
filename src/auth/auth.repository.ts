a6445dddec92cb8b81f253adfde4ba938ff0d0cc
yjcho <jinjanic91@gmail.com>
2022-05-05 23:53:58 +0900

    feat. password 암호화 done

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -5,12 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/authCredential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class AuthRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (err) {
